599056b22bf995210f7cfa375bba1762e04b8a4f
Kyle Pryor <kylepryor@Kyles-Air.columbus.rr.com>
2022-10-18 01:43:18 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
+import { HomeComponent } from './home/home.component';
+import { EventsComponent } from './events/events.component';
+import { SalesComponent } from './sales/sales.component';
+import { IgxNavbarModule, IgxButtonModule, IgxIconModule, IgxButtonGroupModule } from 'igniteui-angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    HeaderBarComponent,
+    HomeComponent,
+    EventsComponent,
+    SalesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    IgxNavbarModule,
+    BrowserAnimationsModule,
+    IgxButtonModule,
+    IgxIconModule,
+    IgxButtonGroupModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
