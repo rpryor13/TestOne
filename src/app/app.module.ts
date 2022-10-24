6a48abe1152557b4b3eab1c9ea0561fd5a1723bf
Kyle Pryor <kylepryor@Kyles-Air.columbus.rr.com>
2022-10-24 01:33:00 -0400

    Added inventory page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 import { HeaderBarComponent } from './header-bar/header-bar.component';
 import { HomeComponent } from './home/home.component';
 import { EventsComponent } from './events/events.component';
 import { SalesComponent } from './sales/sales.component';
 import { IgxNavbarModule, IgxButtonModule, IgxIconModule, IgxButtonGroupModule } from 'igniteui-angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InventoryComponent } from './inventory/inventory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
     HeaderBarComponent,
     HomeComponent,
     EventsComponent,
-    SalesComponent
+    SalesComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
