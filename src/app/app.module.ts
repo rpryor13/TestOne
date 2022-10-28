7218cd0fc1aa5afdaef9abe492304b7a97fd625e
Austin Costa <Austin.costa98@gmail.com>
2022-10-27 20:24:43 -0400

    Added routing
    Added routing functionality to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { SalesComponent } from './sales/sales.component';
 import { InventoryComponent } from './inventory/inventory.component';
 import { BounceHouseComponent } from './bounce-house/bounce-house.component';
 import { BounceHouseCreateComponent } from './bounce-house-create/bounce-house-create.component';
+import { AngularFireModule } from "@angular/fire/compat";
 
 @NgModule({
   declarations: [
@@ -38,6 +39,17 @@ import { BounceHouseCreateComponent } from './bounce-house-create/bounce-house-c
     IgxButtonGroupModule,
     HttpClientModule,
     AppRoutingModule,
+    AngularFireModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCZ30nJV86YNwb4tJugNk110dZenMffLBU",
+      authDomain: "vernal-design-313916.firebaseapp.com",
+      databaseURL: "https://vernal-design-313916-default-rtdb.firebaseio.com",
+      projectId: "vernal-design-313916",
+      storageBucket: "vernal-design-313916.appspot.com",
+      messagingSenderId: "174449647637",
+      appId: "1:174449647637:web:092bc08da262e4cd1d2e1c",
+      measurementId: "G-81XNEJE3PG"
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
